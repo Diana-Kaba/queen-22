edcc42b9568cb4a12d81ecdcfa040a84ef4dff48
miwanoff <marina.iva.noffa@gmail.com>
2023-01-22 12:22:31 +0200

    Add start functionality

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let turn = 1;
 let number = 0;
 
+let cardsField = document.getElementById("cards");
+let info = document.getElementById("info");
+let cardNumber = document.getElementById("card");
+let setCard = document.getElementById("set_card");
+let rel = document.getElementById("reload");
+
+cardNumber.value = 0;
+info.innerHTML = "Take the card!";
+
 function shuffle(arr) {
   let rand, temp;
   for (let i = 0; i < arr.length; i++) {
@@ -16,32 +25,34 @@ function shuffle(arr) {
 
 shuffle(cards);
 
-alert(cards);
+cardsField.innerHTML = cards;
 
-function play(cards) {
-  function checkWin(who, card) {
-    alert(card);
-    if (card == "Q") {
-      alert(who + " win");
-      return true;
-    }
-    return false;
-  }
-  let card;
-  while (cards.length) {
-    if (turn % 2 != 0) {
-      number = +prompt("Take the card! 0-" + cards.length);
-      if (isNaN(number) || (number > cards.length && number < 0)) return;
-      let card = cards[number];
-      if (checkWin("You ", card)) return;
-    } else {
-      number = Math.floor(Math.random(cards.length) + 1);
-      let card = cards[number];
-      if (checkWin("I ", card)) return;
-    }
-    cards.splice(number, 1);
-    alert(cards);
-    turn++;
-  }
-}
-play(cards);
+// alert(cards);
+
+// function play(cards) {
+//   function checkWin(who, card) {
+//     alert(card);
+//     if (card == "Q") {
+//       alert(who + " win");
+//       return true;
+//     }
+//     return false;
+//   }
+//   let card;
+//   while (cards.length) {
+//     if (turn % 2 != 0) {
+//       number = +prompt("Take the card! 0-" + cards.length);
+//       if (isNaN(number) || (number > cards.length && number < 0)) return;
+//       let card = cards[number];
+//       if (checkWin("You ", card)) return;
+//     } else {
+//       number = Math.floor(Math.random(cards.length) + 1);
+//       let card = cards[number];
+//       if (checkWin("I ", card)) return;
+//     }
+//     cards.splice(number, 1);
+//     alert(cards);
+//     turn++;
+//   }
+// }
+// play(cards);
