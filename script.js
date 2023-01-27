dc7c47e99092009d4301f83ec8464e721bc3cd6b
Diana-Kaba <kabadiana327@gmail.com>
2023-01-27 11:05:43 +0200

    Add confetti and button

--- script.js
+++ script.js
@@ -120,6 +120,7 @@ function checkWin(who, card) {
   info.innerHTML = who + " take " + card;
   if (card == "Q") {
     info.innerHTML = who + " win";
+    confetti();
 
     stop();
     fly();
@@ -148,3 +149,34 @@ window.onload = function () {
   addEventCardList();
   rel.addEventListener("click", newPlay);
 };
+
+function confetti() {
+  $.each($(".particletext.confetti"), function () {
+    var confetticount = ($(this).width() / 50) * 10;
+    for (var i = 0; i <= confetticount; i++) {
+      $(this).append(
+        '<span class="particle c' +
+          $.rnd(1, 2) +
+          '" style="top:' +
+          $.rnd(10, 50) +
+          "%; left:" +
+          $.rnd(0, 100) +
+          "%;width:" +
+          $.rnd(6, 8) +
+          "px; height:" +
+          $.rnd(3, 4) +
+          "px;animation-delay: " +
+          $.rnd(0, 30) / 10 +
+          's;"></span>'
+      );
+    }
+  });
+}
+
+jQuery.rnd = function (m, n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.floor(Math.random() * (n - m + 1)) + m;
+};
+
+initparticles();
